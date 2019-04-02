1b718ffb09ff8a377f273d143aa44ec4f81878b9
Fatuity <nxhvnkl123@gmail.com>
2019-04-02 08:54:39 +0300

    wip

--- ex31.c
+++ ex31.c
@@ -4,8 +4,16 @@
  * Group: 06
  */
 #include <stdio.h>
+#include <unistd.h>
+#include <syscall.h>
+#include <fcntl.h>
 
 int main(int argc, char *argv[]) {
-    printf("Hello, World!\n");
+    if(argc<3){
+        perror("not enough arguments");
+        return 1;
+    }
+    int fl1 = open(argv[1],O_RDONLY);
+    int fl2 = open(argv[2],O_RDONLY);
     return 0;
 }
