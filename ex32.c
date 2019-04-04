95d1e445ec89b2d2e0ea24b5051b1e1398856a7b
Fatuity <nxhvnkl123@gmail.com>
2019-04-05 00:01:43 +0300

    opened configuration file.

--- ex32.c
+++ ex32.c
@@ -1,5 +1,16 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <dirent.h>
+
 /***
  * Name: Daniel Levy
  * ID: 318813714
  * Group: 06
  */
+int main(int argc, char* argv[]){
+    if(argc!=2){
+        perror("not enough arguments.");
+    }
+    int fdConf = open(argv[1],O_RDONLY);
+}
